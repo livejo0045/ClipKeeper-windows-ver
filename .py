e7270f883cac294b05539a64c2fbc43f7cfaec46
Jonas <livejo0045@gmail.com>
2026-04-12 00:18:26 +0100

    Update .py

---
+++ .py
@@ -0,0 +1,12 @@
+import sys
+import random
+from datetime import datetime 
+from PyQt6.QtWidgets import (
+     Application, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QPushButton, QScrollArea, QFrame, SizePolicy, QGraphicsOpacityEffect, QMenu )
+from PyQt6.QtCore import(
+    Qt, QTimer, QPropertyAnimation, QEasingCurve, QSize, QMineData, pyqtSignal, QThread, QObject)
+from PyQt6.QtGui import(
+    QColor, QPalette, QPixmap, QImage, QFont, QFontDatabase, QIcon, QPainter, QBrush, QPen, QLinearGradient, QCursor, QAction )
+)
+import uuid
+
